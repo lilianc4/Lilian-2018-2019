a76125ce04bf72229b8440426dc998d1aef2e70a
mchen <mchen@mchen-jchv2n>
2018-08-21 18:20:03 -0700

    First Commit, Hello World!

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author (Lilian)
+ * @version (APCS3rd, 2018-2019)
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Lilian"); //Display the string
+    }
+}
+
