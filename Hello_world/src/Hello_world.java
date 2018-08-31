fe724fd37f226bd7070e8f06b9fb8e3b5a2bf406
mchen <mchen@10.61.90.103>
2018-08-31 10:56:29 -0700

    ok

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -1,13 +1,15 @@
 /**
  * 
- * @author (Lilian)
+ * @author (Lilian Chang)
  * @version (APCS3rd, 2018-2019)
+ * 8/24/18
+ * prints out what ever is in the system.out.println, displays the string
  */
 public class Hello_world
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From Lilian"); //Display the string
+        System.out.println("Hello World! From Lilian");
     }
 }
 
