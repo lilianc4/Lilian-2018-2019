11fc1716832999dd77bd64ee93c27251913ae590
mchen <mchen@10.61.90.103>
2018-09-04 10:21:21 -0700

    Hi！

--- pikachu/src/Pikachu.java
+++ pikachu/src/Pikachu.java
@@ -7,9 +7,9 @@ public class Pikachu {
 
 	public static void main(String[] args) {
 		System.out.println("Pikachu welcomes you to the world of Pokemon!");
-		System.out.println("		(\\__/)");
-		System.out.println("		(o^.^)")	
-		System.out.println
+		System.out.println("		(\\_ _/)");
+		System.out.println("		(o^.^ )");	
+		System.out.println("	      z(_(\")(\")");
 	}
 
 }
